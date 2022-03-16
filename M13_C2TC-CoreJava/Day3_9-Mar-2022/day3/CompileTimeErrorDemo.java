678fd8725513168719444af3127ede9ab72628b3
bhavesh <bhavesh@bhavesh>
2022-03-17 01:42:29 +0530

    First Commit

---
+++ M13_C2TC-CoreJava/Day3_9-Mar-2022/day3/CompileTimeErrorDemo.java
@@ -0,0 +1,14 @@
+// Compile time error program 
+
+package day3;
+
+class CompileTimeErrorDemo
+{	
+	   public static void main(String [] args) 
+	   {
+		   int x;
+		   in y;  // wrong data type declaration
+		   System.out.println(" Hello Everyone");
+		   System.out.println("My name is Aniket") // missing semicolon(;) syntax error
+	   }	
+}
---
+++ M13_C2TC-CoreJava/Day3_9-Mar-2022/day3/RunTimeErrorDemo.java
@@ -0,0 +1,12 @@
+package day3;
+
+class RunTimeErrorDemo
+{	
+	public static void main(String [] args)
+	{	
+		int n1 = 1;
+		int n2 = 0;
+		System.out.println(n1/n2); // Runtime error: Divide by zero exception
+	}	
+	  
+}
