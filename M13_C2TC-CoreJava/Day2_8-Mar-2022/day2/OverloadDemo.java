032c008c331ff0b6bf50c80965e40ddaa10f46d9
bhavesh <bhavesh@bhavesh>
2022-03-17 01:03:18 +0530

    First Commit

--- M13_C2TC-CoreJava/Day2_8-Mar-2022/day2/OverloadDemo.java
+++ M13_C2TC-CoreJava/Day2_8-Mar-2022/day2/OverloadDemo.java
@@ -4,15 +4,15 @@ package day2;
 
 class Overload
 {
-    void area(int x)
+    void area(int x) // method area with one argument
     {
         System.out.println("the area of the square is "+x*x+" sq units");
     }
-    void area(int x, int y)
+    void area(int x, int y) // method area with more than one argument with same data type
     {
         System.out.println("the area of the rectangle is "+x*y+" sq units");
     }
-    void area(double x)
+    void area(double x) // method area with different data type
     {
         double z = 3.14 * x * x;
         System.out.println("the area of the circle is "+z+" sq units");
