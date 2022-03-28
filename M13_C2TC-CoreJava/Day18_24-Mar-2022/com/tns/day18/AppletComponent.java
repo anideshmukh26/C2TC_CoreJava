db51e5e344788460534dd30c4d5f7cc60629abd2
bhavesh <bhavesh@bhavesh>
2022-03-29 03:39:35 +0530

    First Commit

---
+++ M13_C2TC-CoreJava/Day18_24-Mar-2022/com/tns/day18/AppletComponent.java
@@ -0,0 +1,49 @@
+// program to demonstrate that displays several awt components
+
+package com.tns.day18;
+
+import java.applet.Applet;
+import java.awt.Button;
+import java.awt.TextField;
+import java.awt.Label;
+import java.awt.Checkbox;
+import java.awt.CheckboxGroup;
+import java.awt.Choice;
+
+public class AppletComponent extends Applet
+{
+	Button btn;
+	TextField tf;
+	Label lbl;
+	Checkbox cb;
+	CheckboxGroup cbg;
+	Choice ch;
+	
+	public void init()
+	{
+		Button btn = new Button(" This is Button ");
+		add(btn);
+		
+		Label lbl = new Label("This is Label");
+		add(lbl);
+		
+		TextField tf = new TextField(" This is TextField ",20);
+		add(tf);
+		
+		Checkbox cb = new Checkbox(" This is Checkbox ");
+		add(cb);
+		
+		CheckboxGroup cbg = new CheckboxGroup();
+		add(new Checkbox("CB Item 1", cbg, false));
+		add(new Checkbox("CB Item 2", cbg, true));
+		add(new Checkbox("CB Item 3", cbg, false));
+		
+		Choice ch = new Choice();
+		ch.addItem("Choice Item 1");
+		ch.addItem("Choice Item 2");
+		ch.addItem("Choice Item 3");
+		add(ch);
+		
+		setSize(300,300);
+	}
+}
