4f455ea8f55855b0b6bd9f9d618289329419d307
bhavesh <bhavesh@bhavesh>
2022-04-08 05:21:44 +0530

    First Commit

--- M13_C2TC-CoreJava/Day1_7-Mar-2022/HelloWorld.java
+++ M13_C2TC-CoreJava/Day1_7-Mar-2022/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld
 {
 	public static void main(String[] args) //main method
 	{
-		System.out.println(" Hello World..!! ");
+		System.out.println(" Hello World..!! ");		// prints message
 	}
 }
