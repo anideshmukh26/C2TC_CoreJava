a5ae6de719f017e8aba3886dfd4305947489b15c
bhavesh <bhavesh@bhavesh>
2022-05-14 01:06:20 +0530

    First Commit

--- M13_C2TC-CoreJava/Day66_11-May-2022/com/tns/day66/StudentStatementResultSetDemo.java
+++ M13_C2TC-CoreJava/Day66_11-May-2022/com/tns/day66/StudentStatementResultSetDemo.java
@@ -5,41 +5,27 @@ package com.tns.day66;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 
 public class StudentStatementResultSetDemo
 {
-	public static void main(String args[])
+	public static void main(String[] args) throws Exception
 	{
-		try
-		{
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/b3tns","root","1234");
 			
 			Statement st = con.createStatement();
-			String query = "select * from student";
-			ResultSet rs = st.executeQuery(query);
 			
-			ResultSetMetaData rsmt = rs.getMetaData();
-			System.out.println(rsmt);
-			int column = rsmt.getColumnCount();
-			
+			ResultSet rs = st.executeQuery("Select studentId, studentName, studentMarks from student");
 			while(rs.next())
 			{
-				for(int i= 1; i<=column; i++)
-				{
-					System.out.println(rs.getString(i)+ "\t");
-				}
-				System.out.println("");
+				System.out.println(rs.getString(1)); // first column
+				System.out.println(rs.getString(2)); // Second column
+				System.out.println(rs.getString(3)); // Third  column
 			}
+			
 			st.close();
 			con.close();
-		}
-		catch(Exception e)
-		{
-			System.out.println(e);
-		}
-	}	
+	}
 }
---
+++ M13_C2TC-CoreJava/Day66_11-May-2022/com/tns/day66/StudentStatementResultSetMetaDataDemo.java
@@ -0,0 +1,45 @@
+
+
+package com.tns.day66;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+
+public class StudentStatementResultSetMetaDataDemo
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/b3tns","root","1234");
+			
+			Statement st = con.createStatement();
+			String query = "select * from student";
+			ResultSet rs = st.executeQuery(query);
+			
+			ResultSetMetaData rsmt = rs.getMetaData();
+			System.out.println(rsmt);
+			int column = rsmt.getColumnCount();
+			
+			while(rs.next())
+			{
+				for(int i= 1; i<=column; i++)
+				{
+					System.out.println(rs.getString(i)+ "\t");
+				}
+				System.out.println("");
+			}
+			st.close();
+			con.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}	
+}
